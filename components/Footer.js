e2cc6b700c873fc862266a28535625e184debffe
as203145 <112533924+as203145@users.noreply.github.com>
2022-08-31 13:27:51 +0530

    Initial Commit

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+       Biswas R&amp;D Network | NOC Contact: <a href="mailto:noc@as203145.com">noc@as203145.com</a>
       </footer>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>AS203145 | Biswas R&amp;D Network</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to AS203145" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Stay tuned! We will be right back.
         </p>
       </main>
 
